0dde543a51583c8ffc11b24c9ecd2f867f84a6fa
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2017-12-11 10:02:51 +0100

    Added day 11 project

---
+++ 11/index.js
@@ -0,0 +1,4 @@
+function closeModal() {
+    document.getElementById('modal-info').setAttribute('style', 'display: none');
+    document.getElementById('user').setAttribute('universal-controls', '');
+}
